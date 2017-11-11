ef42eece80adc14df19e2d6e3b488d4abae8be71
root <root@debian>
2017-11-11 12:34:41 +0100

    Modification de la page d'accueil

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Timer } from '../../app/timer';
+import { State } from '../../app/state';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	private btnPlay: string = 'Start';
+	
+	private timer: Timer = new Timer();
+	private state: State = new State();
+	
+	constructor(public navCtrl: NavController) {}
 
-  }
+	play() {
+		this.timer.start();
+		this.state.setPlay();
+		this.btnPlay = 'Continue';
+	}
 
+	stop() {
+		this.timer.stop();
+		this.state.setStop();
+	}
+	
+	backward() {
+		this.timer.reset();
+		this.state.setBackward();
+		this.btnPlay = 'Start';
+	}
 }
